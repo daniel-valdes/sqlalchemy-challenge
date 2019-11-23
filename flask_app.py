f64a17949fe1509b0458c0a8ec0a71eb3a42bf08
Daniel Valdes <53982849+daniel-valdes@users.noreply.github.com>
2019-11-23 17:05:07 -0500

    final commit

--- flask_app.py
+++ flask_app.py
@@ -31,7 +31,7 @@ app = Flask(__name__)
 @app.route("/")
 def Home():
     return(
-        f"<h3>Available Routes:</h3>"
+        f"<h3>Available Routes:<hr></h3>"
         f"/api/v1.0/precipitation</br>"
         f"/api/v1.0/stations</br>"
         f"/api/v1.0/tobs</br>"
